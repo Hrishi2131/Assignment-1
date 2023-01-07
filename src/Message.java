a2a9360cc874ffe49246d25ff6b3e0d8807d5a48
Hrishikesh <rushikeshsonawane2500@gmail.com>
2023-01-07 14:01:42 +0530

    My first git

---
+++ src/Message.java
@@ -0,0 +1,10 @@
+
+public class Message {
+
+public static void main(String[] args) {
+
+System.out.println("Velocity Corporate training center"); 	
+
+}
+}
+
---
+++ src/Message_.java
@@ -0,0 +1,8 @@
+
+public class Message_ {
+
+	public static void main(String[] args) {
+	System.out.println ("Velocity Corporation traning center");
+	}
+	
+}
